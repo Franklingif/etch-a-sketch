cfa54a6dfcae52211d3206d2cf01e392fd5ed584
Franklin <franklin.manhood@gmail.com>
2025-03-24 12:26:27 +0100

    Hover effect completed for one color
    I managed to make the code leave a trail when going throught a case with js, so the change is permanent.

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -5,3 +5,11 @@ for (let i = 0; i < 16 * 16; i++) {
     square.classList.add("square"); // Ajoute la classe CSS
     container.appendChild(square); // Ajoute le div dans le container
 }
+
+const squares = document.querySelectorAll('.square');  // Sélectionne tous les éléments avec la classe 'square'
+
+squares.forEach(square => {
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = 'yellowgreen'; // Change la couleur au survol de chaque carré
+    });
+});
